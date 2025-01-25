e43f2d276686803d6b58e4c982268530cc78883b
flessner_ <dev@flessner.io>
2025-01-25 17:00:50 +0100

    removed umami

--- website/src/entry-server.tsx
+++ website/src/entry-server.tsx
@@ -8,7 +8,6 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <script defer src="https://umami.flessner.dev/script.js" data-website-id="e9502472-0fa4-484d-937d-02f00dbc4d59" />
           {assets}
         </head>
         <body>
