14b8b0c70c4df12f53b79d5cdfa75164e066507d
flessner_ <dev@flessner.io>
2025-02-11 06:26:22 +0100

    new website ui?

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -12,17 +12,27 @@
 		afterNavigate(() => posthog.capture("$pageview"));
 	}
 
-	// scroll position
 	let scrollY = $state(0);
+	let compact = $derived(scrollY > 50);
 </script>
 
 <svelte:window bind:scrollY />
 
 <div
-	class={`absolute z-50 top-0 h-20 w-full duration-300 ease-in-out pointer-events-none ${scrollY > 1 ? "backdrop-blur-2xl" : ""}`}
+	class={`fixed top-0 w-full backdrop-blur-2xl pb-2 duration-500 ease-in-out hover:pb-1`}
 >
-	<div class="h-full max-w-xl mx-auto px-4 flex flex-row">
-		<a href="/" class="my-auto pointer-events-auto">flessner.dev</a>
+	<div
+		class={`px-4 flex flex-row justify-between bg-background-alt overflow-hidden outline-border outline-4 duration-500 ease-in-out hover:outline-primary hover:outline-offset-0 ${compact ? "h-12 outline-offset-4" : "h-24 outline-offset-8"}`}
+	>
+		<div class="flex flex-col -gap-2 justify-center select-none">
+			<a href="/" class="h-5 hover:underline">Torben Flessner</a>
+			<p
+				class={`duration-500 ease-in-out ${compact ? "h-0 translate-y-5" : "h-5"}`}
+			>
+				Student @
+				<a class="hover:underline" href="https://www.kit.edu/">KIT</a>
+			</p>
+		</div>
 	</div>
 </div>
 
--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,85 +1,21 @@
-<script lang="ts">
-    import { onMount } from "svelte";
-
-    const { data } = $props();
-    const { size, offsetX, offsetY } = data;
-
-    // dynamic state
-    let clientHeight = $state(0);
-    let clientWidth = $state(0);
-
-    let amountX = $derived(Math.floor(clientWidth / size) + 4);
-    let amountY = $derived(Math.floor(clientHeight / size) + 4);
-
-    // mouse position
-    let mouseY = $state(-500);
-    let mouseX = $state(-500);
-    let lastMove = $state(Date.now());
-
-    function handleMove(event: MouseEvent | TouchEvent) {
-        if (event instanceof MouseEvent) {
-            mouseX = event.clientX;
-            mouseY = event.clientY;
-        } else if (event instanceof TouchEvent) {
-            mouseX = event.touches[0].clientX;
-            mouseY = event.touches[0].clientY;
-        }
-
-        // reset if 1000ms have passed
-        lastMove = Date.now();
-        setTimeout(() => {
-            if (Date.now() - lastMove > 495) {
-                mouseX = -500;
-                mouseY = -500;
-            }
-        }, 500);
-    }
-
-    // prevent scrolling on body - fixes mobile scrolling
-    onMount(() => {
-        const body = document.querySelector("body");
-        body.classList.add("overflow-hidden");
-
-        return () => {
-            body.classList.remove("overflow-hidden");
-        };
-    });
-</script>
-
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<div
-    class="relative h-[100svh] w-screen overflow-hidden"
-    onmousemove={handleMove}
-    ontouchmove={handleMove}
-    bind:clientHeight
-    bind:clientWidth
->
-    <div class="relative h-full w-full overflow-scroll z-100">
-        <!-- TBD content that needs to be scrolled -->
-    </div>
-    {#each [...Array(amountX).keys()] as x}
-        {#each [...Array(amountY).keys()] as y}
-            {@render Pixel(x, y)}
-        {/each}
-    {/each}
+<div class="h-500 mt-40 max-w-xl mx-auto">
+    <p class="text-foreground/70 italic">
+        website under construction, content coming soon
+    </p>
+    <li class="mt-4">
+        <a
+            href="mailto:torben@flessner.dev"
+            class="text-primary hover:underline"
+        >
+            email
+        </a>
+    </li>
+    <li>
+        <a
+            href="https://github.com/flessner"
+            class="text-primary hover:underline"
+        >
+            github
+        </a>
+    </li>
 </div>
-
-<!-- pixel component for the header -->
-{#snippet Pixel(x: number, y: number)}
-    {@const letter = Math.random().toString(36).substring(2, 3)}
-    {@const distance = Math.sqrt(
-        (mouseY - y * size - offsetY) ** 2 + (mouseX - x * size - offsetX) ** 2,
-    )}
-
-    {@const effect = Math.max(0, 100 - distance)}
-
-    {@const effectX = Math.random() - 0.5}
-    {@const effectY = Math.random() - 0.5}
-
-    <div
-        class={`absolute z-10 w-[32px] h-[32px] flex items-center justify-center duration-200`}
-        style={`top: ${y * size - size / 2 + offsetY}px; left: ${x * size - size / 2 + offsetX}px; transform: translate(${effectX * effect}px, ${effectY * effect}px);`}
-    >
-        <p class="text-foreground/10 font-mono text-xs select-none">{letter}</p>
-    </div>
-{/snippet}
